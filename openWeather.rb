4dea6518ab35ddc1c71fcdaa33b925885d4a8838
Keith Lee <51897022+Abrakebabra@users.noreply.github.com>
2021-02-09 17:25:51 +0900

    Finished first draft of openWeather puller

--- openWeather.rb
+++ openWeather.rb
@@ -10,6 +10,28 @@
 "current"
 "hourly"
 "daily"
+
+
+functions
+
+Hourly vs daily
+function to enter in target hour
+takes the average wind speed of the 6 hours after
+takes the average precipitation percentage
+takes average humidity
+
+if 6 hours are not available, just take the average of whatever it can get
+if the target hour is not on the hourly list, grab the wind forecast from the daily forecast
+Optional if have time:  If target date is not on hourly list, go straight to daily without checking hours
+ - maybe later.  Get it working first.
+Grab the first and last available weather description.  If not the same, have "xx description to yy description"
+grab the first and last available wind direction.  Same thing.  More useful than average direction.
+
+daily
+from 3rd day out, just grab daily data only
+
+Sun
+grab sunrise sunset times for each day
 =end
 
 
@@ -60,9 +82,122 @@ class OpenWeatherMap
   end
 
 
-  def getForecastFromTime
+  # source: http://snowfence.umn.edu/Components/winddirectionanddegrees.htm
+  def windDegToCardinal(deg)
+    if deg < 11.25
+      return "N"
+    elsif deg < 33.75
+      return "NNE"
+    elsif deg < 56.25
+      return "NE"
+    elsif deg < 78.75
+      return "ENE"
+    elsif deg < 101.25
+      return "E"
+    elsif deg < 123.75
+      return "ESE"
+    elsif deg < 146.25
+      return "SE"
+    elsif deg < 168.75
+      return "SSE"
+    elsif deg < 191.25
+      return "S"
+    elsif deg < 213.75
+      return "SSW"
+    elsif deg < 236.25
+      return "SW"
+    elsif deg < 258.75
+      return "WSW"
+    elsif deg < 281.25
+      return "W"
+    elsif deg < 303.75
+      return "WNW"
+    elsif deg < 326.25
+      return "NW"
+    elsif deg < 348.75
+      return "NNW"
+    else
+      return "N"
+    end
+  end #windDegToCardinal
 
 
+  def msToKph(ms)
+    return (ms * 3.6).round(1)
+  end
+
+
+  def avgOfArray(array)
+    return (array.sum / array.length).round(1)
+  end
+
+
+  # returns nil if not found
+  # string input
+  def getForecastFromHourly(yymmddhh)
+    @calledJSON["hourly"].each.with_index do |hour, index|
+      if yymmddhh == convertJST(hour["dt"]).slice(0..8)
+        windSpeeds = []
+        windDirections = [] #first and last
+        precipitation = []
+        humidity = []
+        weatherDescriptions = [] #first and last
+
+        for i in index...index + 6
+          if @calledJSON["hourly"][i].nil?
+            break
+          end
+
+          windSpeeds.push(msToKph(@calledJSON["hourly"][i]["wind_speed"]))
+          windDirections.push(@calledJSON["hourly"][i]["wind_deg"])
+          humidity.push(@calledJSON["hourly"][i]["humidity"])
+          precipitation.push(@calledJSON["hourly"][i]["pop"])
+          weatherDescriptions.push(@calledJSON["hourly"][i]["weather"][0]["description"])
+        end
+
+        windAvg = avgOfArray(windSpeeds)
+        windDirFirstLast = [
+          windDegToCardinal(Integer(windDirections[0])),
+          windDegToCardinal(Integer(windDirections[windDirections.length - 1]))]
+        precipitationAvg = avgOfArray(precipitation)
+        humidityAvg = avgOfArray(humidity)
+        weatherDescrFirstLast = [weatherDescriptions[0], weatherDescriptions[weatherDescriptions.length - 1]]
+
+        return {
+          "forecastType" => "hourly",
+          "windAvg" => windAvg, "windDirFirstLast" => windDirFirstLast,
+          "precipitationAvg" => precipitationAvg, "humidityAvg" => humidityAvg,
+          "weatherDescrFirstLast" => weatherDescrFirstLast
+        }
+      end # if hour is found
+    end #look in each hourly entry
+    # if yymmddhh not found
+    return nil
+  end
+
+
+  def getForecastFromDaily(yymmdd)
+    @calledJSON["daily"].each do |day|
+        if yymmdd == convertJST(day["dt"]).slice(0...6)
+          return {
+            "forecastType" => "daily",
+            "wind" => msToKph(day["wind_speed"]),
+            "windDir" => windDegToCardinal(Integer(day["wind_deg"])),
+            "precipitation" => day["pop"],
+            "humidity" => day["humidity"],
+            "weatherDescription" => day["weather"][0]["description"]
+          }
+
+        end #if date found
+    end #for each day
+    #day not found
+    raise StandardError.new "Target date not found in forecast data"
+  end # getDailyForecastFrom
+
+
+
+
+  def getForecastTest
     #hourly first
     @calledJSON["hourly"].each do |hourly|
       jst = convertJST(hourly["dt"])
@@ -80,18 +215,31 @@ class OpenWeatherMap
   end
 
 
+  def getForecastFromYYMMDDHH(yymmddhh)
+    forecast = getForecastFromHourly(yymmddhh)
+    if forecast.nil?
+      forecast = getForecastFromDaily(yymmddhh.slice(0...6))
+    end
+    if forecast.nil?
+      raise StandardError.new "Date not found in forecast"
+    end
+    return forecast
+  end
+
 
 
   def printCalledJSON
     puts @calledJSON
   end
 
-
 end
 
 owm = OpenWeatherMap.new
-#owm.authorize
-#owm.oneCall
-owm.testCall
-owm.getForecastFromTime
+owm.authorize
+owm.oneCall
+#owm.testCall
+#owm.getForecastTest
 #owm.printCalledJSON
+#puts owm.getForecastFromDaily("210209")
+#puts owm.getForecastFromHourly("21020915")
+puts owm.getForecastFromYYMMDDHH("21021019")
